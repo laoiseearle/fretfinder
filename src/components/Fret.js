41bb4fb687e5631f8d605e71f47eb930c71b0d31
Laoise Earle <laoiseearle92@gmail.com>
2020-09-07 15:29:06 +0100

    Create frets

---
+++ src/components/Fret.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Note from './Note';
+import './Fret.css';
+
+function Fret() {
+  return (
+    <>
+      <div className="fret">
+        <Note />
+      </div>
+    </>
+  );
+}
+
+export default Fret;
