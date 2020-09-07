68bc4aca7eef09ce40b2b0728bd531bf1893a262
Laoise Earle <laoiseearle92@gmail.com>
2020-09-07 15:28:52 +0100

    Create strings

---
+++ src/components/String.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Fret from './Fret';
+import './String.css';
+
+function String() {
+  return (
+    <div className="string">
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+      <Fret />
+    </div>
+  );
+}
+
+export default String;
