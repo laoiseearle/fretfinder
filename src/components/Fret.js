dc0680ad44fecb397060766f837a0e023ca2f7f8
Laoise Earle <laoiseearle92@gmail.com>
2020-11-22 14:26:44 +0000

    Add color to notes

--- src/components/Fret.js
+++ src/components/Fret.js
@@ -19,9 +19,38 @@ function Fret({ note, index, updateTuning, notesArray, string, flattenPitch }) {
     }
   };
 
+  const noteColor = () => {
+    switch (note) {
+      case 'C': {
+        return '#fd2424';
+      }
+      case 'D': {
+        return '#24fd48';
+      }
+      case 'E': {
+        return '#fd3fd4';
+      }
+      case 'F': {
+        return '#fde03f';
+      }
+      case 'G': {
+        return '#174fe9';
+      }
+      case 'A': {
+        return '#f58849';
+      }
+      case 'B': {
+        return '#09cbe0';
+      }
+      default: {
+        return 'white';
+      }
+    }
+  };
+
   return (
     <div className="fret">
-      <div className="note" onClick={changeNote}>
+      <div className="note" style={{ backgroundColor: noteColor() }} onClick={changeNote}>
         {note}
       </div>
     </div>
