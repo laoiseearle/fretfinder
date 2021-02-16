87efcfb5fda43937b3b9d2aa6048c5505780a296
Laoise Earle <laoiseearle92@gmail.com>
2021-02-16 21:13:51 +0000

    Updates

---
+++ precache-manifest.e07bdf3b5577d0ab0fce27e958dee008.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4d1d0130a32da9906e5f38e22121a0",
+    "url": "/fretfinder/index.html"
+  },
+  {
+    "revision": "2c96ab1b7ff8e2558db5",
+    "url": "/fretfinder/static/css/2.37dbfa43.chunk.css"
+  },
+  {
+    "revision": "f6e01d787c0e0733bc71",
+    "url": "/fretfinder/static/css/main.40c8ec50.chunk.css"
+  },
+  {
+    "revision": "2c96ab1b7ff8e2558db5",
+    "url": "/fretfinder/static/js/2.2cf03174.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fretfinder/static/js/2.2cf03174.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e01d787c0e0733bc71",
+    "url": "/fretfinder/static/js/main.c8a98be3.chunk.js"
+  },
+  {
+    "revision": "8b9d4a6c54a62ad35c3d",
+    "url": "/fretfinder/static/js/runtime-main.a3c132da.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fretfinder/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/fretfinder/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fretfinder/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fretfinder/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fretfinder/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
